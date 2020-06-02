13082f5a6dba86db72a2b6a54c7cf70449def788
LibLev <libardi.levente@gmail.com>
2020-06-02 11:10:06 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import './App.css';
 import Login from './component/login/login';
 import Registration from './component/login/registration';
 
 class App extends Component {
-  render(){
-  return (
-    <div>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/login" component={Login}/>
-          <Route path="/registration" component={Registration}/>
-        </Switch>
-      </BrowserRouter>
-    </div>
-    
-  );
-}
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/registration" component={Registration}/>
+                    </Switch>
+                </BrowserRouter>
+            </div>
+
+        );
+    }
 }
 
 export default App;
