ce7aca9ecefbe5505a827f5e63fb5daaeea0cc5b
LibLev <libardi.levente@gmail.com>
2020-06-04 14:07:54 +0200

    statement page style in progress

--- src/component/photo/photoUploader.jsx
+++ src/component/photo/photoUploader.jsx
@@ -2,11 +2,11 @@ import React, {Component} from "react";
 import axios from "axios";
 
 
-class PhotoUploader extends Component{
+class PhotoUploader extends Component {
 
     state = {
         selectedFiles: null,
-        imgUris: [],
+        imgUris: "",
         ariaValue: 0
     }
 
@@ -14,19 +14,17 @@ class PhotoUploader extends Component{
         let files = event.target.files;
         const formData = new FormData;
         const names = [];
-        for (let i = 0; i < files.length; i++) {
-            formData.append("images", files[i], files[i].name);
-            names.push(files[i].name);
-        }
+        formData.append("files", files, files.name);
+        names.push(files.name);
         this.setState({selectedFiles: formData});
         this.setState({imgUris: names})
     };
 
     fileUploadHandler = async () => {
         await axios.post("https://localhost:5001/api/ImageUpload",
-            this.state.selectedFiles,{
+            this.state.selectedFiles, {
                 onUploadProgress: progressEvent => {
-                    this.setState({ariaValue: Math.round(progressEvent.loaded/ progressEvent.total *100)})
+                    this.setState({ariaValue: Math.round(progressEvent.loaded / progressEvent.total * 100)})
                 }
             }
         ).then(resp => {
@@ -41,11 +39,13 @@ class PhotoUploader extends Component{
                 <form>
                     <div className="form-group">
                         <label htmlFor="exampleFormControlFile1">Képek kiválasztása</label>
-                        <input type="file" className="form-control-file" id="exampleFormControlFile1" multiple onChange={this.fileSelectedHandler}/>
+                        <input type="file" className="form-control-file" id="exampleFormControlFile1" multiple
+                               onChange={this.fileSelectedHandler}/>
                         <button className="btn btn-dark" onClick={this.fileUploadHandler}>Képek feltöltése</button>
                     </div>
                     <div className="progress">
-                        <div className="progress-bar bg-success" role="progressbar" style={{width: this.state.ariaValue+"%"}}
+                        <div className="progress-bar bg-success" role="progressbar"
+                             style={{width: this.state.ariaValue + "%"}}
                              aria-valuenow={this.state.ariaValue} aria-valuemin="0" aria-valuemax="100"/>
                     </div>
                     <div className="form-group">
@@ -54,8 +54,9 @@ class PhotoUploader extends Component{
                     </div>
                 </form>
             </div>
-    )
+        )
     }
 
-    }
-    export default PhotoUploader;
+}
+
+export default PhotoUploader;
