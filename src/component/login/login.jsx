1d407f80612676c7a378745b5cf754121e46acf8
pakayb <pakayb@gmail.com>
2020-06-18 15:17:38 +0200

    Modify axios to communicate with backend api

--- src/component/login/login.jsx
+++ src/component/login/login.jsx
@@ -1,66 +1,104 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import axios from "axios";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 class Login extends Component {
-    state = {
-        username: "",
-        password: "",
-    };
+  state = {
+    username: "",
+    password: "",
+  };
 
-    usernameOnChange = (event) => {
-        this.setState({username: event.target.value});
-    };
+  usernameOnChange = (event) => {
+    this.setState({ username: event.target.value });
+  };
 
-    passwordOnChange = (event) => {
-        this.setState({password: event.target.value});
-    };
+  passwordOnChange = (event) => {
+    this.setState({ password: event.target.value });
+  };
 
-    login = () => {
-        axios.post(`http://localhost:8080/auth/signin`,
-            {
-                username: this.state.username,
-                password: this.state.password
-            }
-        ).then(resp => {
-            console.log(resp.data);
-            localStorage.setItem("token", resp.data.token);
-            this.setState({redirect: true});
-        }).catch((e) => {
-            console.log(e.message)
-        })
+  login = () => {
+    axios
+      .post(`https://localhost:44314/api/Login`, {
+        UserName: this.state.username,
+        Password: this.state.password,
+      })
+      .then((resp) => {
+        console.log(resp);
+        console.log(resp.headers);
+        localStorage.setItem("token", resp.data.token);
+        this.setState({ redirect: true });
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  };
 
-    };
-
-    render() {
-        return (
-            <div className="container" style={{marginLeft: "auto", marginRight: "auto", marginTop: "5%", width: "28rem"}}>
-                <div className="card">
-                    <div className="card-body">
-                        <form>
-                            <div className="form-group">
-                                <label htmlFor="exampleInputEmail1">Email address/Username</label>
-                                <input type="text" className="form-control" id="exampleInputEmail1"
-                                       aria-describedby="emailHelp" onChange={this.usernameOnChange} value={this.state.username}/>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="exampleInputPassword1">Password</label>
-                                <input type="password" className="form-control" id="exampleInputPassword1"/>
-                            </div>
-                            <div className="form-group form-check">
-                                <input type="checkbox" className="form-check-input" id="exampleCheck1" onChange={this.passwordOnChange} value={this.state.password}/>
-                                <label className="form-check-label" htmlFor="exampleCheck1">Stay logged in</label>
-                            </div>
-                            <button className="btn btn-dark" onClick={this.login}>Login</button>
-                        </form>
-                    </div>
-                    <div className="card-footer text-muted">
-                        <span>If you don't registered yet <a href="/registration">click here!</a></span>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div
+        className="container"
+        style={{
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: "5%",
+          width: "28rem",
+        }}
+      >
+        <div className="card">
+          <div className="card-body">
+            <form>
+              <div className="form-group">
+                <label htmlFor="exampleInputEmail1">
+                  Email address/Username
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  onChange={this.usernameOnChange}
+                  value={this.state.username}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleInputPassword1">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  onChange={this.passwordOnChange}
+                  value={this.state.password}
+                />
+              </div>
+              <div className="form-group form-check">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label className="form-check-label" htmlFor="exampleCheck1">
+                  Stay logged in
+                </label>
+              </div>
+              <button
+                className="btn btn-dark"
+                onClick={this.login}
+                type="button"
+              >
+                Login
+              </button>
+            </form>
+          </div>
+          <div className="card-footer text-muted">
+            <span>
+              If you don't registered yet{" "}
+              <a href="/registration">click here!</a>
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
--- src/component/login/registration.jsx
+++ src/component/login/registration.jsx
@@ -1,68 +1,107 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import axios from "axios";
 
-class Registration extends Component{
+class Registration extends Component {
+  state = {
+    username: "",
+    password: "",
+    email: "",
+  };
 
-    state = {
-        username : "",
-        password : "",
-        email : ""
-    }
+  usernameOnChange = (event) => {
+    this.setState({ username: event.target.value });
+  };
 
-    usernameOnChange = (event) => {
-        this.setState({username: event.target.value})
-    }
+  passwordOnChange = (event) => {
+    this.setState({ password: event.target.value });
+  };
 
-    passwordOnChange = (event) => {
-        this.setState({password:event.target.value})
-    }
+  emailOnChange = (event) => {
+    this.setState({ email: event.target.value });
+  };
 
-    emailOnChange = (event) => {
-        this.setState({email: event.target.value})
-    }
+  registration = () => {
+    axios
+      .post("https://localhost:44314/api/Users", {
+        UserName: this.state.username,
+        Password: this.state.password,
+        Email: this.state.email,
+      })
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  };
 
-    registration = () => {
-        axios.post("http://localhost:8080/registration", {
-            username : this.state.username,
-            password : this.state.password,
-            email : this.state.email
-        }).then((response) => {
-            console.log(response);
-        }).catch((e)=>{
-            console.log(e.message);
-        })
-    }
-
-    render(){
-        return(
-            <div className="container" style={{marginLeft: "auto", marginRight: "auto", marginTop: "5%", width: "28rem"}}>
-                <div className="card">
-                    <div className="card-body">
-                        <form>
-                            <div className="form-group">
-                                <label htmlFor="exampleInputEmail1">Email address</label>
-                                <input type="email" className="form-control" id="exampleInputEmail1"
-                                       aria-describedby="emailHelp" onChange={this.emailOnChange} value={this.state.email}/>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="exampleInputPassword1">Username</label>
-                                <input type="text" className="form-control" id="exampleInputUsername" onChange={this.usernameOnChange} value={this.state.username}/>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="exampleInputPassword1">Password</label>
-                                <input type="password" className="form-control" id="exampleInputPassword1" onChange={this.passwordOnChange} value={this.state.password}/>
-                            </div>
-                            <div className="form-group form-check">
-                                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                                <label className="form-check-label" htmlFor="exampleCheck1">I read <a href="/#">terms & conditions</a>!</label>
-                            </div>
-                            <button type="submit" className="btn btn-dark">Registration</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-
-        )
-    }
+  render() {
+    return (
+      <div
+        className="container"
+        style={{
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: "5%",
+          width: "28rem",
+        }}
+      >
+        <div className="card">
+          <div className="card-body">
+            <form>
+              <div className="form-group">
+                <label htmlFor="exampleInputEmail1">Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  onChange={this.emailOnChange}
+                  value={this.state.email}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleInputPassword1">Username</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="exampleInputUsername"
+                  onChange={this.usernameOnChange}
+                  value={this.state.username}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleInputPassword1">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  onChange={this.passwordOnChange}
+                  value={this.state.password}
+                />
+              </div>
+              <div className="form-group form-check">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label className="form-check-label" htmlFor="exampleCheck1">
+                  I read <a href="/#">terms & conditions</a>!
+                </label>
+              </div>
+              <button
+                type="button"
+                className="btn btn-dark"
+                onClick={this.registration}
+              >
+                Registration
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default Registration;
