2f7dd0653db74887e04307f85eada700f8f577a4
pakayb <pakayb@gmail.com>
2020-08-04 11:06:16 +0200

    Add avatar group and modify pagination, get the total number of records in db, and the current rows of the page

--- src/component/pages/reportsPage.jsx
+++ src/component/pages/reportsPage.jsx
@@ -9,6 +9,7 @@ import TableRow from "@material-ui/core/TableRow";
 import TablePagination from "@material-ui/core/TablePagination";
 import Paper from "@material-ui/core/Paper";
 import Avatar from "@material-ui/core/Avatar";
+import AvatarGroup from "@material-ui/core/Avatar";
 import axios from "axios";
 
 const useStyles = makeStyles({
@@ -20,8 +21,10 @@ const useStyles = makeStyles({
 export default function SimpleTable() {
   const classes = useStyles();
   const [page, setPage] = React.useState(0);
+  const [numOfRows, setNumOfRows] = React.useState(0);
   const [rows, setRows] = React.useState([]);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
@@ -31,33 +34,48 @@ export default function SimpleTable() {
     setPage(0);
   };
 
-  const getData = async () => {
-    await axios.get("https://localhost:5001/api/Reports").then((resp) => {
-      console.log(resp.data);
-      setRows(resp.data);
-    });
+  const getData = async (page, rowsPerPage) => {
+    var Url = "https://localhost:5001/api/Reports?page="
+      .concat(page)
+      .concat("&size=" + rowsPerPage);
+    return await axios.get(Url);
+  };
+
+  const memorizeData = React.useCallback(() => {
+    getNumberOfRows().then((resp) => setNumOfRows(resp.data));
+    getData(page, rowsPerPage).then((resp) => setRows(resp.data));
+  }, [page, rowsPerPage]);
+
+  const getNumberOfRows = async () => {
+    return await axios.get(
+      "https://localhost:5001/api/Reports/numberOfReports"
+    );
   };
 
   React.useEffect(() => {
-    getData();
-  }, []);
+    memorizeData();
+  }, [memorizeData]);
 
   const emptyRows =
-    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+    rowsPerPage - Math.min(rowsPerPage, numOfRows - page * rowsPerPage);
 
   const createAvatars = function (images) {
     const src = "http://127.0.0.1:8887/";
     if (images === []) {
       return "no picture";
     }
-    return images.map((image) => {
-      return (
-        <Avatar
-          alt={image.imgaeName}
-          src={src.concat(image.imageName)}
-        ></Avatar>
-      );
-    });
+    return (
+      <AvatarGroup max={4}>
+        {images.map((image) => {
+          return (
+            <Avatar
+              alt={image.imgaeName}
+              src={src.concat(image.imageName)}
+            ></Avatar>
+          );
+        })}
+      </AvatarGroup>
+    );
   };
 
   return (
@@ -73,23 +91,21 @@ export default function SimpleTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows
-            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            .map((row, index) => (
-              <TableRow
-                key={row.name}
-                // Implement modal, with the details of the report, images and a map with a marker
-                onClick={() => alert("Leendő részletes ablak")}
-              >
-                <TableCell component="th" scope="row">
-                  {row.id}
-                </TableCell>
-                <TableCell align="right">{row.longitude}</TableCell>
-                <TableCell align="right">{row.latitude}</TableCell>
-                <TableCell align="right">{row.comment}</TableCell>
-                <TableCell align="right">{createAvatars(row.images)}</TableCell>
-              </TableRow>
-            ))}
+          {rows.map((row, index) => (
+            <TableRow
+              key={row.name}
+              // Implement modal, with the details of the report, images and a map with a marker
+              onClick={() => alert("Leendő részletes ablak")}
+            >
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell align="right">{row.longitude}</TableCell>
+              <TableCell align="right">{row.latitude}</TableCell>
+              <TableCell align="right">{row.comment}</TableCell>
+              <TableCell align="right">{createAvatars(row.images)}</TableCell>
+            </TableRow>
+          ))}
           {emptyRows > 0 && (
             <TableRow style={{ height: 53 * emptyRows }}>
               <TableCell colSpan={6} />
@@ -100,7 +116,7 @@ export default function SimpleTable() {
       <TablePagination
         rowsPerPageOptions={[5, 10, 25]}
         component="div"
-        count={rows.length}
+        count={numOfRows}
         rowsPerPage={rowsPerPage}
         page={page}
         onChangePage={handleChangePage}
